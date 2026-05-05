4cae110f5a9ee5c89766fc805ed2b39c30d0827b
anoul <anaelbailly.contact@gmail.com>
2026-05-05 23:26:33 +0200

    Update SHA256 — fix menubar reopen after settings

--- Casks/picontrol.rb
+++ Casks/picontrol.rb
@@ -1,6 +1,6 @@
 cask "picontrol" do
   version "1.0.0"
-  sha256 "711fea3a0f1b606443b2bd6e43177a7196e3f0b1593ddeaccda0e11e29332367"
+  sha256 "266735a9070a444147ca11c56ce1e315d5fde2d683bc0dd9818e95ed3a5c0990"
 
   url "https://github.com/Anoulpainting/picontrol/releases/download/v#{version}/PiControl.dmg"
   name "PiControl"
